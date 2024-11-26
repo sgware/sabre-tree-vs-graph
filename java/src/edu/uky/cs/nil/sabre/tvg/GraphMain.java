8b0a46f909f26b15f873b1b5d8f4e358bb6b74f0
Nabuat <146083701+Nabuat@users.noreply.github.com>
2024-11-25 23:46:21 -0500

    Update GraphMain.java

--- java/src/edu/uky/cs/nil/sabre/tvg/GraphMain.java
+++ java/src/edu/uky/cs/nil/sabre/tvg/GraphMain.java
@@ -6,20 +6,19 @@ public class GraphMain {
 	 * 
 	 * @param args name of the problem, then the ATL and CTL respectively
 	 */
-    public static void main(String[] args) {
-        if (args.length != 3) {
-            System.err.println("Usage: java GraphMain <problem> <ATL> <CTL>");
-            System.exit(1);
-        }
-
-        String problem = args[0];
-        int atl = Integer.parseInt(args[1]);
-        int ctl = Integer.parseInt(args[2]);
-
-        GraphTest g = new GraphTest();
-        g.filePath = problem;
-        g.minutes = 1;
-        System.out.println("Running GRAPH TEST on " + problem);
-        g.callGraphTest();
-    }
+	public static void main(String[] args) {
+		if (args.length != 3) {
+			System.err.println("Usage: java GraphMain <problem> <ATL> <CTL>");
+			System.exit(1);
+		}
+		String problem = args[0];
+		int atl = Integer.parseInt(args[1]);
+		int ctl = Integer.parseInt(args[2]);
+		
+		GraphTest g = new GraphTest();
+		g.filePath = problem;
+		g.minutes = 1;
+		System.out.println("Running GRAPH TEST on " + problem);
+		g.callGraphTest();
+	}
 }
